778d89a8cdd89fd13a07aedc7beb48bb166d6418
meenakshi gupta <guptamunmun45@gmail.com>
2023-05-06 22:38:37 +0530

    commited index.jade

---
+++ public/DB/db.js
@@ -0,0 +1,6 @@
+const mongodb=require('mongodb')
+const dbname='Pdf_editor'
+const dburl=`mongodb+srv://funupdb-first:VxaFh8Uez4zyv95l@cluster0.kizeuyb.mongodb.net/PDF?retryWrites=true&w=majority`
+
+
+module.exports={mongodb,dbname,dburl}
